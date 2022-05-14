2109ad2caf92da31e3adb1b155f76481b4766849
Andrey Zakharchenko <a_zakharchenko_a@mail.ru>
2022-05-14 19:55:16 +0300

    First commit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        List<Integer> list = Arrays.asList(1, 2, 5, 16, -1, -2, 0, 32, 3, 5, 8, 23, 4);
+        List<Integer> result = new ArrayList<>();
+
+        for(int i : list) {
+            if (i > 0 && i % 2 == 0) {
+                result.add(i);
+            }
+        }
+        Collections.sort(result);
+        System.out.println(result);
+    }
+}
